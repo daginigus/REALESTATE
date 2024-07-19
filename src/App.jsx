97e93ff7d173024bb7387585122aec4d5636ee7b
Dagmawi <dagmawinigussie6@example.com>
2024-07-19 16:01:28 +0300

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>APP</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/sign-in" element={<Signin />} />
+  <Route path="/sign-up" element={<Signup />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/profile" element={<Home />} />
+
+  </Routes>
+  </BrowserRouter>;
 }
---
+++ src/pages/signin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function signin() {
+  return (
+   <h1>signin</h1>
+  )
+}
